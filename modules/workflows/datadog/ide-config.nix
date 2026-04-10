dfe4a598e21c1f98420ed24d4b204a26bb1c45a3
Richard Morrill <richard.morrill@datadoghq.com>
2026-04-10 11:08:20 -0400

    more vscode extensions

--- modules/workflows/datadog/ide-config.nix
+++ modules/workflows/datadog/ide-config.nix
@@ -3,6 +3,11 @@
 {
   programs.vscode.profiles.default.extensions = with pkgs.vscode-marketplace; [
     datadog.datadog-vscode
+
+    # These ones ones are more for just working with Bazel in general
+    # TODO: Consider migrating to central Bazel workflow
+    bazelbuild.vscode-bazel
+    golang.go
   ];
   programs.vscode.profiles.default.userSettings = {
       datadog.connection.notification.suppressSignInNotification = true;
