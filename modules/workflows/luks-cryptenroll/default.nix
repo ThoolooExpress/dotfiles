fd2fa562579137fd59d0ea5680937db1f669c4cc
Richard Morrill <morrill@thoolooexpress.com>
2026-05-31 09:15:59 -0400

    enable more secure secure boot

--- modules/workflows/luks-cryptenroll/default.nix
+++ modules/workflows/luks-cryptenroll/default.nix
@@ -12,7 +12,7 @@ let
   enrollLines = lib.concatStringsSep "\n" (
     lib.mapAttrsToList (
       _name: uuid:
-      ''sudo systemd-cryptenroll --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs="0+7+15:sha256=0000000000000000000000000000000000000000000000000000000000000000" /dev/disk/by-uuid/${uuid}''
+      ''sudo systemd-cryptenroll --wipe-slot=tpm2 --tpm2-device=auto --tpm2-pcrs="0+2+7+11+15:sha256=0000000000000000000000000000000000000000000000000000000000000000" /dev/disk/by-uuid/${uuid}''
     ) cfg.devices
   );
 
